b6a570bdf0365ce397dc363b6ee813d0384f1c1c
CooperMiller0089 <tskmcious@gmail.com>
2025-06-05 18:15:50 +0800

    Create text_utils.py

---
+++ text_utils.py
@@ -0,0 +1,7 @@
+def count_words(text):
+    """Count the number of words in a string."""
+    return len(text.split())
+
+def reverse_text(text):
+    """Return the reversed string."""
+    return text[::-1]
